edc73f117c7bc2fc5f7c09799a116418b53eae7b
vitor1234567789 <120030239+vitor1234567789@users.noreply.github.com>
2023-11-25 11:24:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-VJo2vI8pnKxZCPvASBvbL3Km7sz3NDM",
+    authDomain: "projeto-85878.firebaseapp.com",
+    projectId: "projeto-85878",
+    storageBucket: "projeto-85878.appspot.com",
+    messagingSenderId: "285530394150",
+    appId: "1:285530394150:web:9c2da3c3afc1fd0f57a0e7"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
